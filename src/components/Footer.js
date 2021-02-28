a35b1d93109ea9ec9a0622ad0bbfb625bf33bc67
Daniela Vazquez <danielavazquez95@gmail.com>
2021-02-28 01:10:17 -0300

    FIX : css responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,12 @@ export const Footer = () => {
 
     return (
          
-        <div className="footer bg-dark py-4 mt-1">
-            <div className=" text-center p-2">
+        <div className="footer bg-dark mt-2">
+            <div className=" text-center p-3">
                 <h5 className="text-white d-inline">Created and designed by Daniela Vazquez - ©2021</h5> 
                 <br/>
                 <label className="text-white d-inline text-secondary"> Buenos Aires - Argentina </label>                                                   
-                <hr className="w-50"/>
+                <hr/>
             </div>
         </div>
     )
